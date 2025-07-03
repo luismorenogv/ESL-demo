97079108405d366af1ae456b3c71f79752b3f42f
l-provenzano <l.provenzano@student.utwente.nl>
2025-07-03 04:15:47 +0200

    Finished testing, merged html files

--- Pi/controller/steps2rads.h
+++ Pi/controller/steps2rads.h
@@ -16,11 +16,16 @@ extern "C" {
 #include <math.h>
 #include "xxtypes.h"
 
-XXDouble steps2rads(int32_t steps, int32_t max_steps)
+// Converts a number of steps to radians based on the maximum steps and total angle in radians.
+XXDouble steps2rads(int32_t steps, int32_t max_steps, XXDouble total_angle_rad)
 {
-    return (2*M_PI*steps)/max_steps;
+    // Handle division by zero
+    if (max_steps == 0) {
+        return 0.0;
+    }
+    // Calculate the ratio of travel (0.0 to 1.0) and scale by the total angle
+    return ((XXDouble)steps / (XXDouble)max_steps) * total_angle_rad;
 }
-
 #ifdef __cplusplus
 }
 #endif
--- Pi/test/CPP/test_steps2rads.c
+++ Pi/test/CPP/test_steps2rads.c
@@ -1,26 +1,34 @@
+#include <gtest/gtest.h>
 #include "../../controller/steps2rads.h"
 #include <math.h>
-#include <gtest/gtest.h>
 
 void setUp(void) {}
 void tearDown(void) {}
 
 
-void test_steps2rads_zero_steps_returns_zero(void)
+TEST(steps2rads_test, nullTest)
 {
     int32_t steps = 0;
     int32_t max_steps = 1000;
-    XXDouble result = steps2rads(steps, max_steps);
+    XXDouble result = steps2rads(steps, max_steps, M_PI);
 
     EXPECT_EQ(0.0, result);
 }
 
 
-void test_steps2rads_half_circle(void)
+TEST(steps2rads_test, halfwayTest)
 {
     int32_t steps = 500;
     int32_t max_steps = 1000;
-    XXDouble result = steps2rads(steps, max_steps);
+    XXDouble result = steps2rads(steps, max_steps, M_PI);
 
-    EXPECT_NEAR(result, M_PI/2. 1e-6);
+    EXPECT_NEAR(result, M_PI/2, 1e-6);
+}
+
+TEST(steps2rads_test, divisionByZero)
+{
+    int32_t steps = 500;
+    XXDouble result = steps2rads(steps, 0, M_PI);
+
+    EXPECT_NEAR(result, 0, 1e-6);
 }
